b2cf2d3c0e521650217c69e865de3460989bb3ac
misosviso <56072239+misosviso@users.noreply.github.com>
2022-05-18 11:00:06 +0200

    Netlify Plugin - generate netlify functions

--- plugins/@iteria-netlify-plugin/generateNetlifyFunctions.js
+++ plugins/@iteria-netlify-plugin/generateNetlifyFunctions.js
@@ -445,14 +445,6 @@ exports.generateNetlifyFunctions = () => {
     fs.mkdir('./netlify/functions', (err) => {
 
       console.log('Directory created netlify/functions successfully!');
-      fs.writeFileSync(
-        './netlify/functions/ExecuteCommitAddition.ts',
-        executeCommitAddition
-      );
-      fs.writeFileSync(
-        './netlify/functions/GetFetchHeadOid.ts',
-        getFetchHeadOid
-      );
 
       fs.mkdir('./netlify/functions/netlifyGraph', (err) => {
         if (err) {
@@ -462,7 +454,18 @@ exports.generateNetlifyFunctions = () => {
           'Directory netlify/functions/netlifyGraph created successfully!'
         );
         fs.writeFileSync('./netlify/functions/netlifyGraph/index.js', indexJs);
-      });
+    });
+
+      fs.writeFileSync(
+        './netlify/functions/GetFetchHeadOid.ts',
+        getFetchHeadOid
+      );
+
+      fs.writeFileSync(
+        './netlify/functions/ExecuteCommitAddition.ts',
+        executeCommitAddition
+      );
+
     });
   });
 };
