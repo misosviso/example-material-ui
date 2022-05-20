9fb2b591833b407e5f29504ceb14d690dac4f106
misosviso <michal.komora@protonmail.com>
2022-05-20 08:52:48 +0200

    resolving merge coonflict

--- src/layouts/DashboardLayout/index.tsx
+++ src/layouts/DashboardLayout/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { makeStyles } from '@mui/styles';
@@ -58,64 +57,3 @@ const DashboardLayout = () => {
 };
 
 export default DashboardLayout;
-=======
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
-import { makeStyles } from '@mui/styles';
-import NavBar from './NavBar';
-import TopBar from './TopBar';
-
-const useStyles = makeStyles((theme: any) => ({
-  root: {
-    backgroundColor: theme.palette.background.dark,
-    display: 'flex',
-    height: '100%',
-    overflow: 'hidden',
-    width: '100%'
-  },
-  wrapper: {
-    display: 'flex',
-    flex: '1 1 auto',
-    overflow: 'hidden',
-    paddingTop: 64,
-    [theme.breakpoints.up('lg')]: {
-      paddingLeft: 256
-    }
-  },
-  contentContainer: {
-    display: 'flex',
-    flex: '1 1 auto',
-    overflow: 'hidden'
-  },
-  content: {
-    flex: '1 1 auto',
-    height: '100%',
-    overflow: 'auto'
-  }
-}));
-
-const DashboardLayout = () => {
-  const classes = useStyles();
-  const [isMobileNavOpen, setMobileNavOpen] = useState(false);
-
-  return (
-    <div className={classes.root}>
-      {/* // @ts/ignore */}
-      <TopBar className="" onMobileNavOpen={() => setMobileNavOpen(true)} />
-      <NavBar
-        onMobileClose={() => setMobileNavOpen(false)}
-        openMobile={isMobileNavOpen}
-      />
-      <div className={classes.wrapper}>
-        <div className={classes.contentContainer}>
-          <div className={classes.content}>
-            <Outlet />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default DashboardLayout;
->>>>>>> e65eb09a7c0fb53ac5afbde27e978b71b784ece9
